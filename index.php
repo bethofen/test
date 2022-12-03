bc15617818b6a7c9b3c86b001d7c74faa938b6de
beethofen <gootobhomdgtb@gmail.com>
2022-12-04 03:48:04 +0700

    intinity

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>zhongling</title>
+	<link rel="stylesheet" href="css-zhong.css" href="css-born.css">
+	<link rel="stylesheet"  href="css-born.css">
+</head>
+
+<body>
+	<div class="sceen">
+	<div class="cloud"></div>
+		
+		
+		<div class="float"></div>
+		
+		<div class="metreo" ></div>	
+	      	<div class="effectboom" ></div>	
+		<div class="zhonglingx">
+		<img src="web/zhongling.png">
+		
+         </div>
+ <br><br><br><br>
+	<center>	<h1>beatking sitex</h1></center>
+	
+</body>
+</html>
